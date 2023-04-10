0cb39ab8f3805a6add813143529a96717cb13740
NagolRellis <logansillermeritt@gmail.com>
2023-04-10 13:36:02 -0700

    first commit

---
+++ back-end/src/app.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
+const express = require("express");
+const cors = require("cors");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+const reservationsRouter = require("./reservations/reservations.router");
+const tablesRouter = require("./tables/tables.router");
+
+const app = express();
+
+app.use(cors());
+// app.options("*", cors());
+app.use(express.json());
+
+const router = express.Router();
+router.get("/", cors(), (req, res) => {
+  res.json({
+    message: "Welcome! You can access the data using these routes:",
+  });
+});
+
+app.use("/", router);
+
+app.use("/reservations", reservationsRouter);
+app.use("/tables", tablesRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
